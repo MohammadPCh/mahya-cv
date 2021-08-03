7bf3dda42d319ab613d228bb89b2bbbba76028c2
Pourcheriki <pourcheriki@gmail.com>
2021-08-04 00:39:09 +0430

    change to Cherik

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,14 +54,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://mpch.ir"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by{' Cherik'}
         </a>
       </footer>
     </div>
